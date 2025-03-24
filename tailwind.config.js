0a76f845abf9ef0b345c950dd8e307f2540c2ab8
Kevin Osayabawmen <officialskymas@gmail.com>
2025-03-25 00:18:42 +0100

    improved-homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-∑
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './script.js', './style.css', './**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-yellow': '#FFC107',
+      },
+      backgroundImage: {
+        fruits: "url('/images/fruits.png')",
+      },
+    },
+  },
+  plugins: [],
+};
