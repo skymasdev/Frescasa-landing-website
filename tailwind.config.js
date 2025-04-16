49c2b835f6f6a001c2a26bfe4e87efd4c8ee15ba
Kevin Osayabawmen <officialskymas@gmail.com>
2025-04-16 20:43:55 +0200

    updates on dependecies and change style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './script.js', './style.css', './**/*.html'],
+  content: [
+    './index.html',
+    './script.js',
+    './style.css',
+    './**/*.html',
+    './node_modules/preline/dist/preline.css',
+  ],
   theme: {
     extend: {
       colors: {
